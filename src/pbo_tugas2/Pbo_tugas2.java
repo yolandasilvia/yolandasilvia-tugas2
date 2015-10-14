dfa637380592a7112b9d9a2d835b9cafeb57dd77
yolandasilvia <yolandasilvia68@yahoo.com>
2015-10-14 09:41:16 +0700

    tugas2

---
+++ src/pbo_tugas2/Pbo_tugas2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_tugas2;
+
+/**
+ *
+ * @author user
+ */
+public class Pbo_tugas2 {
+public int nilaihasil ;
+
+double pembagian (double nilai1, double nilai2){
+    double nilaihasil;
+      return  nilaihasil= nilai1 / nilai2;
+}
+int perkalian (int nilai1, int nilai2){
+    nilaihasil = nilai1*nilai2;
+    return nilaihasil;
+}
+int pengurangan (int nilai1, int nilai2){
+    nilaihasil = nilai1-nilai2;
+    return nilaihasil;
+}
+int penjumlahan (int nilai1, int nilai2){
+    nilaihasil = nilai1+nilai2;
+    return nilaihasil;
+}
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
